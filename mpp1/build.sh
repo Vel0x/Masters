5bd0b4e4958e8e24e6870dec0445f18bc93c9ef7
Vel0x <DaleMyers19@gmail.com>
2014-02-10 14:23:58 +0000

    Changed folder structure to work better with sublime

--- mpp1/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-pdflatex -shell-escape -interaction=nonstopmode mpp1.tex && bibtex mpp1 && pdflatex -shell-escape -interaction=nonstopmode mpp1.tex && pdflatex -shell-escape -interaction=nonstopmode mpp1.tex
--- mpp2/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-pdflatex -shell-escape -interaction=nonstopmode mpp2.tex && bibtex mpp2 && pdflatex -shell-escape -interaction=nonstopmode mpp2.tex && pdflatex -shell-escape -interaction=nonstopmode mpp2.tex
